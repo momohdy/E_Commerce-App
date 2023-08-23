a155d0fef1ab4ec977105fbb53a92be84381f1a9
Mostafa-Mohdy <mostafamohdy03@gmail.com>
2023-08-23 12:56:06 +0300

    Adding build folder

--- server/server.js
+++ server/server.js
@@ -1,48 +1,52 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
-const dotenv = require('dotenv') ;
-const DBconnection = require('./config/DBconnection') ;
-const productRouter = require('./routes/productRoutes') ;
-const userRouter = require('./routes/userRoutes') ;
-const orderRouter = require('./routes/orederRoutes') ;
-// const errorHandlingMiddleware = require('./middleware/errorHandlingMiddleware') ; 
+const dotenv = require("dotenv");
+const DBconnection = require("./config/DBconnection");
+const productRouter = require("./routes/productRoutes");
+const userRouter = require("./routes/userRoutes");
+const orderRouter = require("./routes/orederRoutes");
+// const errorHandlingMiddleware = require('./middleware/errorHandlingMiddleware') ;
 
-app.use(express.json()) 
+app.use(express.json());
 
-dotenv.config() ;
+dotenv.config();
 
 DBconnection();
-
-
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/config/paypal", (req, res) => {
-  //  Note , we dont remove {} yet 
+  //  Note , we dont remove {} yet
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
 
 // router
-app.use("/products" , productRouter) ;
-app.use("/users" , userRouter) ;
-app.use("/orders" , orderRouter) ;
+app.use("/products", productRouter);
+app.use("/users", userRouter);
+app.use("/orders", orderRouter);
 
-// original url error 
+app.use(express.static("client/build"));
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`);
+});
+
+// original url error
 app.use((req, res, next) => {
-  res.status(404)
-  next(new Error( `Not Found ${req.originalUrl}`));
-})
+  res.status(404);
+  next(new Error(`Not Found ${req.originalUrl}`));
+});
 
-// error handler 
+// error handler
 app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).send(err.message)
-})
-
+  console.error(err.stack);
+  res.status(500).send(err.message);
+});
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on ${process.env.NODE_ENV} mode , port ${PORT}`);
+  console.log(
+    `Example app listening on ${process.env.NODE_ENV} mode , port ${PORT}`
+  );
 });
