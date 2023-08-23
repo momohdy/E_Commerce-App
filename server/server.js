9ea0112bc9ddfce9106659b66e38e3de7a9f1108
Mostafa Mohdy <103789273+momohdy@users.noreply.github.com>
2023-08-23 19:16:15 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
 const DBconnection = require("./config/DBconnection");
 const productRouter = require("./routes/productRoutes");
 const userRouter = require("./routes/userRoutes");
@@ -10,7 +10,7 @@ const orderRouter = require("./routes/orederRoutes");
 
 app.use(express.json());
 
-dotenv.config();
+// dotenv.config();
 
 DBconnection();
 
